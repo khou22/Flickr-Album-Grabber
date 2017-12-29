8d6c80aefb8ae39f3f377199cb5fd39eb00ba566
Kevin Hou <kevin.ch.hou@gmail.com>
2017-12-29 13:45:16 -0800

    Fixed bug where scrape wouldn't get partial class match

--- GetAlbums.py
+++ GetAlbums.py
@@ -6,6 +6,7 @@ import io
 from datetime import datetime # For timestamps
 
 outputFileName = "flickrData.js"
+blacklist = ["Design", "Roaracle 2015-2016 T-Shirt Contest"]
 
 ################   Get image urls from source   ################
 data = ""
@@ -13,17 +14,18 @@ with open('page.html', 'r') as file:
     data = file.read()
 
 soup = BeautifulSoup(data, "lxml") # Using lxml parser
-albums = soup.findAll("div", { "class": "view photo-list-album-view requiredToShowOnServer" })
+albums = soup.select('div[class*="view photo-list-album-view "]')
 
 data = []
 
 for album in albums:
     style = album.get("style") # Get the style attribute
+    # print(style)
 
     # Extract image URL
-    startKey = "background-image: url(//"
+    startKey = "background-image: url(\"//"
     startURL = style.find(startKey) + len(startKey) # Just before background-image URL
-    endURL = style.find(".jpg)", startURL) + len(".jpg")  # Get end of URL
+    endURL = style.find(".jpg\")", startURL) + len(".jpg")  # Get end of URL
     imageURL = style[startURL:endURL]
 
     # Get title and link of the album
@@ -31,21 +33,22 @@ for album in albums:
     linkPrepend = "https://www.flickr.com"
     link = linkPrepend + linkTag.get("href")
     title = linkTag.get("title")
-    data.append({ "title": title, "link": link, "image": imageURL })
+
+    if title not in blacklist:
+        data.append({ "title": title, "link": link, "image": imageURL })
 
 
 ################   Output to JS File   ################
 print "{0} albums found".format(data.count)
 try:
     with io.FileIO(outputFileName, 'w') as outputFile: # Writing file and creating file if it doesn't exist
-        print "// Last updated: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print "const flickrData = ["
+        # print "// Last updated: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # print "const flickrData = ["
         outputFile.write("// Last updated: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n") # Timestamp
         outputFile.write("const flickrData = [\n") # Open array
 
         # Cycle through data
         for album in data:
-            print(album)
             outputFile.write("  {\n")
             outputFile.write("    \"title\": \"" + album["title"] + "\",\n")
             outputFile.write("    \"link\": \"" + album["link"] + "\",\n")
@@ -53,7 +56,7 @@ try:
             outputFile.write("  },\n")
 
         outputFile.write("];\n") # Close array
-        print "];"
+        # print "];"
         outputFile.close()
 except IOError as (errno, strerror):
     print "I/O error({0}): {1}".format(errno, strerror)
