8323a7817e42ac84535a100769413e43a5b04138
Kevin Hou <kevin.ch.hou@gmail.com>
2017-12-29 13:01:36 -0800

    Working flickr album scrape

--- GetAlbums.py
+++ GetAlbums.py
@@ -1,26 +1,59 @@
 from bs4 import BeautifulSoup # Module to sort through the html
 import lxml # Module to parse through the html for BeautifulSoup
 import urllib2 # Gets html
-# import webbrowser # This module can control the browser
-#
-# url = "https://www.flickr.com/photos/khou22/albums"
-#
-# try: # Make sure link exists
-#     urllib2.urlopen(url)
-# except urllib2.HTTPError, e:
-#     print(e.code) # Return any errors
-# except urllib2.URLError, e:
-#     print(e.args)
-#
-# response = urllib2.urlopen(url) #Get markdown
-#
-# html = response.read() #Markdown
+import os # Searching through computer directory
+import io
+from datetime import datetime # For timestamps
 
+outputFileName = "flickrData.js"
+
+################   Get image urls from source   ################
 data = ""
 with open('page.html', 'r') as file:
     data = file.read()
 
 soup = BeautifulSoup(data, "lxml") # Using lxml parser
-print(soup.title)
-print(soup.findAll("div", { "class": "view photo-list-album-view requiredToShowOnServer" }))
+albums = soup.findAll("div", { "class": "view photo-list-album-view requiredToShowOnServer" })
 
+data = []
+
+for album in albums:
+    style = album.get("style") # Get the style attribute
+
+    # Extract image URL
+    startKey = "background-image: url(//"
+    startURL = style.find(startKey) + len(startKey) # Just before background-image URL
+    endURL = style.find(".jpg)", startURL) + len(".jpg")  # Get end of URL
+    imageURL = style[startURL:endURL]
+
+    # Get title and link of the album
+    linkTag = album.findAll("a")[0]
+    linkPrepend = "https://www.flickr.com"
+    link = linkPrepend + linkTag.get("href")
+    title = linkTag.get("title")
+    data.append({ "title": title, "link": link, "image": imageURL })
+
+
+################   Output to JS File   ################
+print "{0} albums found".format(data.count)
+try:
+    with io.FileIO(outputFileName, 'w') as outputFile: # Writing file and creating file if it doesn't exist
+        print "// Last updated: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print "const flickrData = ["
+        outputFile.write("// Last updated: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n") # Timestamp
+        outputFile.write("const flickrData = [\n") # Open array
+
+        # Cycle through data
+        for album in data:
+            print(album)
+            outputFile.write("  {\n")
+            outputFile.write("    \"title\": \"" + album["title"] + "\",\n")
+            outputFile.write("    \"link\": \"" + album["link"] + "\",\n")
+            outputFile.write("    \"image\": \"" + album["image"] + "\"\n")
+            outputFile.write("  },\n")
+
+        outputFile.write("];\n") # Close array
+        print "];"
+        outputFile.close()
+except IOError as (errno, strerror):
+    print "I/O error({0}): {1}".format(errno, strerror)
